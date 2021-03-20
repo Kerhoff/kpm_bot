c4d1e618c4f523c17519bff5a95c6ce2fe5c188b
Kerhoff <kerhoffsky@gmail.com>
2021-03-20 16:27:24 +0300

    Fix Delete cards function issue

--- bot.py
+++ bot.py
@@ -6,10 +6,6 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryH
 
 import task_manager
 
-logging.basicConfig(format='%(asctime)s - %(name)s- %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 
 def start(update, context):
     context.bot.send_chat_action(chat_id=update.effective_chat.id,
@@ -19,84 +15,90 @@ def start(update, context):
     else:
         message = "Error loading data"
 
-    bot_keyboard: list = [['Update data'],
-                          ['Completed Stories Cards'],
-                          ['New Stories not in Board'],
-                          ['New Cards not in JIRA']]
+    bot_keyboard: list = [['Update Cards Data'],
+                          ['Completed Cards'],
+                          ['New Stories'],
+                          ['New Cards']]
     reply_markup = ReplyKeyboardMarkup(bot_keyboard, resize_keyboard=True, one_time_keyboard=True)
-    
+
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=message,
                              reply_markup=reply_markup)
 
+
 def check_permissions(user_id: int):
     admin_ids: list = os.environ['ADMINS'].split(':')
-    
 
     if str(user_id) in admin_ids:
         return True
     else:
         return False
 
+
 def update_data(update, context):
     if task_manager.update_data():
-        message="Cards updated"
+        message = "Cards updated"
     else:
-        message="Cards update error"
-    
+        message = "Cards update error"
+
     context.bot.send_message(chat_id=update.effective_chat.id,
-                                text=message)
+                             text=message)
+
 
 def send_completed_cards(update, context):
     message: dict = task_manager.get_completed_cards()
     inline_keyboard = [[InlineKeyboardButton("Delete completed Cards", callback_data="delete_completed_cards")]]
     reply_markup = InlineKeyboardMarkup(inline_keyboard)
-        
+
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=message['message'],
                              reply_markup=reply_markup,
                              parse_mode=ParseMode.HTML)
+
 
 def send_new_stories(update, context):
     message: dict = task_manager.get_open_stories_without_card()
     inline_keyboard = [[InlineKeyboardButton("Create Cards in Inbox", callback_data="create_cards_in_inbox")]]
     reply_markup = InlineKeyboardMarkup(inline_keyboard)
-        
+
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=message['message'],
                              reply_markup=reply_markup,
                              parse_mode=ParseMode.HTML)
 
+
 def send_new_cards(update, context):
     message: dict = task_manager.get_new_cards_without_story()
-    
+
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=message['message'],
                              parse_mode=ParseMode.HTML)
 
+
 def reply_to_message(update, context):
     context.bot.send_chat_action(chat_id=update.effective_chat.id,
                                  action=ChatAction.TYPING)
-    
+
     user_id: int = update.effective_user.id
     if check_permissions(user_id):
         text: str = update.message.text
-        if text == 'Update data':
-            update_data(update, context)   
-        elif text == 'Completed Stories Cards':
+        if text == 'Update Cards Data':
+            update_data(update, context)
+        elif text == 'Completed Cards':
             send_completed_cards(update, context)
-        elif text == 'New Stories not in Board':
+        elif text == 'New Stories':
             send_new_stories(update, context)
-        elif text == 'New Cards not in JIRA':
+        elif text == 'New Cards':
             send_new_cards(update, context)
         else:
             context.bot.send_message(chat_id=update.effective_chat.id,
-                                text=text)
+                                     text=text)
     else:
         message = "You don't have permission to access this bot"
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                    text=message,
-                                    parse_mode=ParseMode.MARKDOWN_V2)
+                                 text=message,
+                                 parse_mode=ParseMode.MARKDOWN_V2)
+
 
 def button_pressed(update, context):
     query = update.callback_query
@@ -111,22 +113,32 @@ def button_pressed(update, context):
     else:
         query.edit_message_text(text=f"Selected option: {query.data}")
 
+
 def help(update, context):
     update.message.reply_text("Use /start to start use this bot.")
+
 
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Sorry, I didn't understand that command.")
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-
 def bot():
-    bot_token = os.environ['BOT_TOKEN']
+    logging.basicConfig(format='%(asctime)s - %(name)s- %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    logger.info("Start load data")
     task_manager.load_data()
+    logger.info("Data loaded")
+
+    logger.info("Start Bot")
+    bot_token = os.environ['BOT_TOKEN']
 
     updater = Updater(token=bot_token, use_context=True)
     dispatcher = updater.dispatcher
@@ -138,8 +150,11 @@ def bot():
     dispatcher.add_handler(CallbackQueryHandler(button_pressed))
     dispatcher.add_error_handler(error)
 
+    logger.info("Bot Started")
+
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == "__main__":
     bot()
--- task_manager.py
+++ task_manager.py
@@ -1,7 +1,8 @@
+import os
+import logging
+
 from interfaces import trello
 from interfaces import jira
-import os
-
 
 JIRA_LINK = os.environ['JIRA_LINK']
 TRELLO_INBOX = os.environ['INBOX_ID']
@@ -40,17 +41,17 @@ def update_data() -> bool:
 def get_completed_cards() -> dict:
     global DATA
     global COMPLETED_CARDS
-    
+
     cards: list = DATA['cards']
     closed_stories: list = DATA['closed_stories']
-    
+
     closed_stories_keys: set = set([story['key'] for story in closed_stories])
     COMPLETED_CARDS = [card for card in cards if card['desc'].split('/')[-1].split(')')[0] in closed_stories_keys]
-    
+
     names_of_comleted_cards: str = ""
     for card in COMPLETED_CARDS:
         names_of_comleted_cards += f"\r\n<a href=\"{card['shortUrl']}\">{card['name']}</a>"
-    
+
     message: str = f"<strong>Cards for deleting</strong>\r\nNum: <strong>{len(COMPLETED_CARDS)}</strong>\r\nNames:{names_of_comleted_cards}"
 
     return {'message': message}
@@ -59,21 +60,23 @@ def get_completed_cards() -> dict:
 def get_new_cards_without_story() -> dict:
     global DATA
     global NEW_CARDS
-    
+
     cards: list = DATA['cards']
     open_stories: list = DATA['open_stories']
-    closed_stories:list = DATA['closed_stories']
-    
+    closed_stories: list = DATA['closed_stories']
+
     open_stories_keys: set = set([story['key'] for story in open_stories])
-    cards_not_in_open_stories: list = [card for card in cards if card['desc'].split('/')[-1].split(')')[0] not in open_stories_keys]
+    cards_not_in_open_stories: list = [card for card in cards if
+                                       card['desc'].split('/')[-1].split(')')[0] not in open_stories_keys]
 
     closed_stories_keys: set = set([story['key'] for story in closed_stories])
-    NEW_CARDS = [card for card in cards_not_in_open_stories if card['desc'].split('/')[-1].split(')')[0] not in closed_stories_keys]
-        
+    NEW_CARDS = [card for card in cards_not_in_open_stories if
+                 card['desc'].split('/')[-1].split(')')[0] not in closed_stories_keys]
+
     new_card_names: str = ""
     for card in NEW_CARDS:
         new_card_names += f"\r\n<a href=\"{card['shortUrl']}\">{card['name']}</a>"
-    
+
     message: str = f"<strong>New Cards (without Story)</strong>\r\nNum: <strong>{len(NEW_CARDS)}</strong>\r\nNames:{new_card_names}"
 
     return {'message': message}
@@ -82,25 +85,28 @@ def get_new_cards_without_story() -> dict:
 def get_open_stories_without_card() -> dict:
     global DATA
     global NEW_STORIES
-    
+
     cards = DATA['cards']
     open_stories = DATA['open_stories']
 
     cards_set: set = set([card['desc'].split('/')[-1].split(')')[0] for card in cards])
     NEW_STORIES = [story for story in open_stories if story['key'] not in cards_set]
-    
+
     story_summaries: str = ""
     for story in NEW_STORIES:
         story_summaries += f"\r\n<a href=\"{JIRA_LINK}/{story['key']}\">{story['fields']['summary']}</a>"
-        
+
     message: str = f"<strong>Stories without Card</strong>\r\nNum: <strong>{len(NEW_STORIES)}</strong>\r\nSummaries:{story_summaries}"
     return {'message': message}
 
 
-def create_new_stories_cards_on_board(new_stories=NEW_STORIES) -> dict:
+def create_new_stories_cards_on_board(new_stories=None) -> dict:
     success = 0
     fail = 0
-    
+
+    if new_stories is None:
+        new_stories = NEW_STORIES
+
     for story in new_stories:
         name = story['fields']['summary']
         key = story['key']
@@ -114,10 +120,14 @@ def create_new_stories_cards_on_board(new_stories=NEW_STORIES) -> dict:
     message = f"Creating result:\r\nSuccess - {success}\r\nFail - {fail}"
     return {'message': message}
 
-def delete_closed_stories_cards(completed_cards=COMPLETED_CARDS) -> dict:
+
+def delete_closed_stories_cards(completed_cards=None) -> dict:
     success: int = 0
     fail: int = 0
-    
+
+    if completed_cards is None:
+        completed_cards = COMPLETED_CARDS
+
     for card in completed_cards:
         delete_card_result: str = trello.delete_card(card['id'])
         if delete_card_result:
@@ -125,5 +135,5 @@ def delete_closed_stories_cards(completed_cards=COMPLETED_CARDS) -> dict:
         else:
             fail += 1
 
-    message: str = f"Delete result:\r\nSuccess - {success}\r\nFail - {fail}"   
+    message: str = f"Delete result:\r\nSuccess - {success}\r\nFail - {fail}"
     return message
